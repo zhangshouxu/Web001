fbe94ad054931f488c1e88e3e9ff8a082980dbef
张守煦 <276238421@qq.com>
2020-02-12 20:27:06 +0800

    修改提交

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * @author Administrator
  * @create 2020-02-11 19:14
  */
-public class Book {
+public class Book{
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+    }
 }
